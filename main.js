c2e709023712498a546fb13523cff3da251e8124
Damian <pokemonmew34567@gmail.com>
2021-09-04 23:24:53 -0700

    done!

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ let previousOperator ="";
 clearbtn.addEventListener('click', () => {
     prevInput.innerHTML = "";
     currentInput.innerHTML = "0";
+    operatorCount = 0;
+    currentOperator = "";
+    previousOperator = "";
 });
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -18,19 +21,30 @@ buttons.forEach(btn => {
             return;
         }
         if(btn.hasAttribute("data-operator")) {
+            prevInput.innerHTML = currentInput.innerHTML;
+            previousOperator = currentOperator;
             currentOperator = btn.innerHTML;
+
             operatorCount++;
         }
 
         if(operatorCount === 2 && btn.hasAttribute("data-operator")) {
-            let inputEquation = currentInput.innerHTML.split(`${btn.innerHTML}`);
+            let inputEquation = currentInput.innerHTML.split(`${previousOperator}`);
+            if(inputEquation[0] == "0" && previousOperator == "÷" && inputEquation[1] == "0") {
+                console.log("oh o!!!");
+                currentInput.innerHTML = "Idiot";
+                return;
+            }
             operatorCount = 1;
             evaluate(inputEquation[0],previousOperator,inputEquation[1]);
-            currentInput.innerHTML = currentInput.textContent + btn.innerHTML;
+            console.log(previousOperator);
+            console.log(currentOperator);
+            //currentInput.innerHTML = currentInput.textContent + btn.innerHTML;
         }
         if(currentInput.innerHTML === "0" && !btn.hasAttribute("data-operator")) { currentInput.innerHTML = ""};
-        currentInput.innerHTML =  currentInput.textContent + btn.innerHTML;
-
+        if(btn.innerHTML != "=") {
+            currentInput.innerHTML =  currentInput.textContent + btn.innerHTML;
+        }
     });
 });
 
@@ -40,7 +54,8 @@ function add(num1, num2) {
 }
 
 function multiply (num1, num2) {
-    return num1 * num2
+    var result = (num1 * num2);
+    return result;
 }
 
 function divide(num1, num2) {
@@ -49,7 +64,8 @@ function divide(num1, num2) {
 }
 
 function subtract(num1, num2) {
-    return num1 - num2
+    var result = num1 - num2;
+    return result;
 
 }
 
@@ -65,15 +81,20 @@ function evaluate(operand1, operator,operand2) {
         
         case 'x':
             let multNum = multiply(operand1, operand2);
+            currentInput.innerHTML = multNum;
+            prevInput.innerHTML = multNum;
             break;
         
         case '-':
             let subNum = subtract(operand1, operand2);
+            currentInput.innerHTML = subNum;
+            prevInput.innerHTML = subNum;
             break;
 
         case '÷':
             let divisNum = divide(operand1, operand2);
-
+            currentInput.innerHTML = divisNum;
+            prevInput.innerHTML = divisNum;
             break;
 
         default:
